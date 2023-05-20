a1bdfd9e363e2c97cb2236075cdba49487902db8
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-05-20 15:23:09 -0700

    debugging undefined

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const math = require('./maths.js')
 
-fs.readFile('maths.js', "utf-8", (error, math) =>
-error ? console.error(error) : console.log(math)
+fs.readFile('maths.js', 'utf-8', (error, math) =>
+error ? console.error(error) : console.log(math.nameQ)
 
 
 );
--- maths.js
+++ maths.js
@@ -1,8 +1,8 @@
 const nameQ = 'What is your name?';
 const projName = 'What is the name of your coding project?';
-const contentTable = 'provide a table of contents if your project requires';
+const contentTable = 'provide a table of contents if your project requires.';
 const installation = 'How do you install your project?';
-const usage = 'Describe the purpose/usage of your project';
+const usage = 'Describe the purpose/usage of your project.';
 const contributors = 'Who contributed to the porject?';
 const instructions = 'Provide instructions about your project.';
 
